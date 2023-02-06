1e177cd5b33a2cce83234150f6efb37c604ce114
BrunoGuerreiro08 <56978723+BrunoGuerreiro08@users.noreply.github.com>
2023-02-06 18:07:51 -0300

    Update Cubo.ino

--- Cubo.ino
+++ Cubo.ino
@@ -5,6 +5,7 @@ int SRCLK_Pin = 13; //pin 11 on the 75HC595 clock
 //How many of the shift registers - change this
 #define number_of_74hc595s 8
 
+//Referentes as entradas do arduino que controlam as layers
 #define t1 2
 #define t2 3
 #define t3 4
@@ -32,11 +33,13 @@ void setup(){
   pinMode(t7, OUTPUT);
   pinMode(t8, OUTPUT);
   Serial.begin(9600);  
-  //reset all register pins
+  
+  //reset all register and layer pins
+  clearLayers();
   clearRegisters();
   writeRegisters();
+  
 }               
-
 
 //set all register pins to LOW
 void clearRegisters(){
@@ -45,16 +48,24 @@ void clearRegisters(){
   }
 } 
 
-//set all register pins to LOW
+//set all register pins to HIGH
 void lightAllRegisters(){
   for(int i = numOfRegisterPins - 1; i >=  0; i--){
      registers[i] = HIGH;
   }
 }
 
+//set all layer pins to LOW
 void clearLayers(){
   for(int i = 2; i < 10; i++){
      digitalWrite(i, LOW);
+  }
+}
+
+//set all layer pins to HIGH
+void lightAllLayers(){
+  for(int i = 2; i < 10; i++){
+     digitalWrite(i, HIGH);
   }
 }
 
@@ -83,23 +94,76 @@ void setRegisterPin(int index, int value){
   registers[index] = value;
 }
 
-/*60
-10
-3 -t3,t4
-7 - t1
-27 -t6
-30 - t7*/
-
 void loop(){
-  CubeCubing(6, 5, 36, 35, 28, 27);
-  delay(300);
-  clearRegisters();
-  writeRegisters();
-  delay(100); 
-  clearLayers(); 
+  
 }
 
-void CubeCubing(int topLayer, int botLayer,int supEsq, int supDir, int infEsq, int infDir){
+//Reproduz a animação de scanner, direçoes aleatórias
+void ScanAnimation(){
+  int side = random(0,7);
+
+  switch (side) {
+  case 1:
+      frontToBack(0);
+    break;
+  case 2:
+      backToFront(56);
+    break;
+  case 3:
+      topToBot();
+    break;
+  case 4:
+      botToTop();
+    break;
+  case 5:
+      leftToRight(0);
+    break;
+  case 6:
+      rightToLeft(7);
+    break;
+  default:
+    // statements
+    break;
+  }
+  clearRegisters();
+  writeRegisters();
+  clearLayers();
+}
+
+//Reproduz a animação de chuva com 3 colunas de leds simultaneamente
+void RainAnimation(){ 
+  int columns[3];
+  for(int i = 0; i < 3; i++){
+    columns[i] = random(0,64);       
+  }
+  
+  for(int i = 0; i < 3; i++){
+    setRegisterPin(columns[i], HIGH);
+  }
+  writeRegisters();
+  
+  for(int i = t8; i >= t1 - 1; i--){
+    digitalWrite(i, HIGH);
+    digitalWrite(i + 1, LOW);
+    digitalWrite(i - 1, LOW);        
+    delay(50);
+  }
+
+  for(int i = 0; i < 3; i++){
+    setRegisterPin(columns[i], LOW);
+  }
+  writeRegisters();
+}
+
+//Reproduz a animação de cubo expandindo e contraindo com um determinado intervalo
+void ExpandAnimation(int delayTime){
+    CubeExpanding(t5, t4, 36, 35, 28, 27, delayTime);
+    delay(delayTime);
+    CubeShrinking(t8, t1,63,56,7,0, delayTime);
+  }
+
+//Funções auxiliares da animação de expansão
+void CubeExpanding(int topLayer, int botLayer,int supEsq, int supDir, int infEsq, int infDir, int delayTime){
     
   if(supEsq < 64){
     digitalWrite(topLayer,HIGH); 
@@ -129,11 +193,136 @@ void CubeCubing(int topLayer, int botLayer,int supEsq, int supDir, int infEsq, i
     }
 
     writeRegisters();         
-    delay(500);
-    CubeCubing(topLayer + 1, botLayer - 1, supEsq + 9, supDir + 7, infEsq - 7, infDir - 9);     
+    delay(delayTime);
+    CubeExpanding(topLayer + 1, botLayer - 1, supEsq + 9, supDir + 7, infEsq - 7, infDir - 9, delayTime);     
   }
 }
 
+void CubeShrinking(int topLayer, int botLayer,int supEsq, int supDir, int infEsq, int infDir, int delayTime){
+    
+  if(supEsq > 36){
+    setRegisterPin(supDir,LOW);
+    setRegisterPin(supEsq,LOW);
+    setRegisterPin(infDir,LOW);
+    setRegisterPin(infEsq,LOW);   
+    writeRegisters();
+    
+    for(int i = infDir; i < infEsq; i++){
+      setRegisterPin(i,LOW);  
+    }
+
+    for(int i = infEsq; i < supEsq;){
+      setRegisterPin(i,LOW);
+      i = i + 8;       
+    }
+
+    for(int i = supDir; i < supEsq; i++){
+      setRegisterPin(i,LOW);  
+    }
+
+    for(int i = infDir; i < supDir;){
+      setRegisterPin(i,LOW);
+      i = i + 8;       
+    }
+      
+    writeRegisters();   
+    digitalWrite(botLayer, LOW);
+    digitalWrite(topLayer, LOW);     
+    delay(delayTime);
+    CubeShrinking(topLayer - 1, botLayer + 1, supEsq - 9, supDir - 7, infEsq + 7, infDir + 9, delayTime);
+  }
+}
+
+//Funções auxiliares da animação de scanner
+void backToFront(int startingPoint){
+  clearRegisters();
+  writeRegisters();
+  lightAllLayers();
+  if(startingPoint >= 0){  
+    for(int i = startingPoint; i < startingPoint + 8; i++){
+      setRegisterPin(i, HIGH);    
+    }
+    writeRegisters();
+    delay(100);
+    backToFront(startingPoint - 8);
+  }   
+}
+
+void frontToBack(int startingPoint){
+  clearRegisters();
+  writeRegisters();
+  lightAllLayers();
+  if(startingPoint < 64){  
+    for(int i = startingPoint; i < startingPoint + 8; i++){
+      setRegisterPin(i, HIGH);    
+    }
+    writeRegisters();
+    delay(100);
+    frontToBack(startingPoint + 8);
+  }   
+}
+
+void rightToLeft(int startingPoint){
+  clearRegisters();
+  writeRegisters();
+  lightAllLayers();
+  if(startingPoint >= 0){  
+    for(int i = startingPoint; i < 64;){
+      setRegisterPin(i, HIGH);
+      i = i+8;    
+    }
+    writeRegisters();
+    delay(100);
+    rightToLeft(startingPoint - 1);
+  }
+}
+
+void leftToRight(int startingPoint){
+  clearRegisters();
+  writeRegisters();
+  lightAllLayers();
+  if(startingPoint < 8){  
+    for(int i = startingPoint; i < 64;){
+      setRegisterPin(i, HIGH);
+      i = i+8;    
+    }
+    writeRegisters();
+    delay(100);
+    leftToRight(startingPoint + 1);
+  }   
+}
+
+void topToBot(){
+  lightAllRegisters();
+  writeRegisters();
+  
+  for(int i = t8; i >= t1 - 1; i--){
+    digitalWrite(i, HIGH);
+    digitalWrite(i + 1, LOW);
+    digitalWrite(i - 1, LOW);        
+    delay(100);
+  }
+  clearRegisters();
+  writeRegisters();
+  clearLayers();
+}
+
+void botToTop(){
+  lightAllRegisters();
+  writeRegisters();
+  
+  for(int i = t1; i <= t8 + 1; i++){
+    digitalWrite(i, HIGH);
+    digitalWrite(i + 1, LOW);
+    digitalWrite(i - 1, LOW);        
+    delay(100);
+  }
+  clearRegisters();
+  writeRegisters();
+  clearLayers();
+}
+
+//Inútil?
 void Around(){
   digitalWrite(t1,HIGH);
    
